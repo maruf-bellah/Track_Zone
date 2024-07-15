4ab9ae8ee0a9bbddb02ca83cdcbd78aad8db3eb5
maruf-bellah <mdmaruf5511507@gmail.com>
2024-07-15 23:40:24 +0600

    useClock setup but not undrastand

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
+import useClock from "./component /hooks/useClock";
+
 const App = () => {
+  const { clock: local } = useClock();
+  const { clock: pst } = useClock("PST");
+
+  console.log("local", local.date);
+  console.log("pst", pst.date);
+
   return (
     <div>
       <h1>Hello Maruf Bella</h1>
